1bc5bf822c95b9f0fbe97e56c41e5eb00cdc33d3
sikkisymoh <sikkisymoh@gamil.com>
2023-09-14 11:59:33 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,9 +7,11 @@
  */
 void print_numbers(void)
 {
-	for (int i = 0; i <= 9; i++)
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
 	{
-		_putchar("%d", i);
+		_putchar(i);
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
