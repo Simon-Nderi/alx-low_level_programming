66e39c84364a97c4afcc69304d62e3bc0a389a70
sikkisymoh <sikkisymoh@gamil.com>
2023-09-16 06:45:23 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,38 +1,48 @@
 #include "main.h"
+
+
 #include <stdio.h>
 
+
 /**
- * print_to_98 - prints all the integers between the number given and 98
- * @n: number to count from
- *
- * Return: void
+ * print_to_98 - print a program to 98
+ * @n: The number to start printing
+ * Return: Always 0.
  */
+
 
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n <= 98)
 	{
-		while (n >= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n--;
-		}
+	for (; n <= 98; n++)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
 	}
 	else
 	{
-		while (n <= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n++;
-		}
+	printf("%d, ", n);
 	}
+	}
+	}
+	else
+	{
+	for (; n >= 98; n--)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
 	printf("\n");
+	break;
+	}
+	else
+	{
+		printf("%d, ", n);
+	}
+	}
+	}
 }
